c7aa1450856a7746db14826d582d9a5cb32a175b
hiromitsu <hitsk26@gmail.com>
2013-10-19 11:29:34 +0900

    12ページ上部まで実行

---
+++ src/Dollar.java
@@ -0,0 +1,13 @@
+
+public class Dollar {
+	int amount ;
+
+	public Dollar(int amount) {
+		this.amount = amount;
+	}
+	void times(int multiplier){
+		amount = amount * 2;
+
+	}
+
+}
---
+++ test/DollarTest.java
@@ -0,0 +1,19 @@
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+
+public class DollarTest {
+
+	@Test
+	public void testMultiplication() {
+		Dollar five = new Dollar(5);
+		five.times(2);
+		assertEquals(10, five.amount);
+
+		five.times(3);
+		assertEquals(15, five.amount);
+	}
+
+
+}
