3b94193ee058e4bb26ee69ff23b7cca7a7156cba
hiromitsu <hitsk26@gmail.com>
2013-10-20 12:03:59 +0900

    9章43pおわりまで

--- src/Dollar.java
+++ src/Dollar.java
@@ -1,13 +1,11 @@
 
 public class Dollar extends Money{
 
-	public Dollar(int amount) {
-		this.amount = amount;
+	public Dollar(int amount,String currency) {
+		super(amount,currency);
 	}
+
 	Money times(int multiplier){
-		return new Dollar(amount * multiplier);
-	}
-	String currency(){
-		return "USD";
+		return Money.dollar(amount * multiplier);
 	}
 }
--- src/Franc.java
+++ src/Franc.java
@@ -1,13 +1,12 @@
 
 public class Franc extends Money{
 
-	public Franc(int amount) {
-		this.amount = amount;
+	public Franc(int amount,String currency) {
+		super(amount,currency);
 	}
+
 	Money times(int multiplier){
-		return new Franc(amount * multiplier);
+		return Money.franc(amount * multiplier);
 	}
-	String currency(){
-		return "CHF";
-	}
+
 }
